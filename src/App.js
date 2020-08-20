2eba4815dccf92b827d7accbeb9a3e75e06c13d0
Mosiur-Rahman-1 <masiurreheman01@gmail.com>
2020-08-21 04:15:51 +0600

    react hook state eventhandelar practice

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const singer = {
+    name: "Mosiur",
+    age: 27,
+    job: "Freelencer"
+  }
+
+  const playerName = ['Shakib al hasan', 'Rubel al kala', 'Nasir the playboy'];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done bro<code>src/App.js</code> and save to reload.
-        </p>
+        <p className= "color">I am {singer.name} {singer.job} person</p>
       </header>
+      <Counter></Counter>
+      <Person name = {playerName[0]}  age = "30"></Person>
+      <Person name = {playerName[1]}  age = "20"></Person>
+      <Person name = {playerName[2]}  age = "28"></Person>
+    </div>
+  );
+}
+
+function Counter () {
+
+  const [count, setCount] = useState(10);
+
+  return (
+
+    <div>
+      <h1>count: {count}</h1>
+      <button onClick = {() => setCount(count+1)}>Increase</button>
+      <button onClick = {() => setCount(count-1)}>DEcrease</button>
+    </div>
+  );
+}
+
+function Person(props) {
+
+
+  return (
+    <div className = "cricketer">
+      <h1>Player Name and title: {props.name}</h1>
+      <p>Bangladeshi cricket player</p>
+  <p>Age: {props.age}</p>
     </div>
   );
 }
